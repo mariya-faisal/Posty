9a996bfa5448b156297bd8b57a387cbc421bac45
Mariya Faisal <mariya.faisal@silkysys.com>
2021-07-24 17:48:21 +0300

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-center">
+
+    <div class="w-50 bg-white p-4 rounded-lg">
+        Posts
+    </div>
+</div>
+
+@endsection
